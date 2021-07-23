4dbb5dc429aeec2ec53861ecff3e891619185f3e
VolkovYury <volkovyurij97@gmail.com>
2021-07-23 10:14:29 +0700

    add lw2.3, lw2.12 and update README

---
+++ lw2.12.c
@@ -0,0 +1,72 @@
+﻿// Функция находит в строке фрагменты, симметричные относительно центрального символа, длиной 7 и более символов (например, "abcdcba") 
+// и возвращает динамический массив указателей на копии таких фрагментов.
+
+// Задание 4. 'l' = 108, 108%9 = 0, 108/9=12, 12 больше количества заданий (12-9 == 3). Задание 3 уже готово, взял следующее
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define MAXSTRING 100
+#define NUMCHAR 3 // количество символов в каждую сторону от центрального. В исходных данных длина искомого фрагмента 7+ (3 слева, центральный, 3 справа) 
+
+int main() {
+    char* locale = setlocale(LC_ALL, "");
+
+    char str[MAXSTRING] = "yjoabcdcbarstqwertrewq\0";
+    int countNum = 0;  // счётчик количества найденных фрагментов
+
+    // выделение памяти под ДМУ. 
+    char** arr = (char**)malloc(sizeof(char*));
+
+    // В условии указано, что искать нужно фрагменты 7+ символов. Если строка меньше такой длины, то и фрагмента там не будет
+    if (strlen(&str) < NUMCHAR*2+1) {
+        printf("Строка меньше %d символов\n", NUMCHAR * 2 + 1);
+        return 1;
+    }
+    else {
+        int count = 0; // счётчик количества симметричных символов (с одной стороны)
+
+        // перебор элементов, но не всех, а лишь тех, которые могут быть центрами симметричных фрагментов
+        for (int i = NUMCHAR; i < strlen(&str) - 1 - NUMCHAR; i++) {
+
+            // подсчёт количества симметричных символов
+            for (int j = 1; str[i + j] == str[i - j]; j++) {
+                count++;
+            }
+
+            // если фрагмент необходимой длины
+            if (count >= NUMCHAR) {
+                
+                // формирование фрагмента
+                char val[MAXSTRING];
+                for (int k = 0; k < count * 2 + 1; k++) {
+                    val[k] = str[i-count+k];
+                }
+                val[count * 2 + 1] = '\0';
+
+                arr[countNum] = (char*) malloc (sizeof(char*)*(strlen(val) + 1));
+
+                strcpy_s(arr[countNum], strlen(val) + 1, val);
+                                
+                countNum++;
+            }
+
+            count = 0;
+        }
+    }
+
+    if (countNum == 0) {
+        printf("Фрагменты в строке не найдены");
+        return 1;
+    }
+    else {
+        printf("Найдено %d фрагментов:\n", countNum);
+        for (int i = 0; i < countNum; i++) {
+            printf("%d фрагмент: %s\n", i + 1, arr[i]);
+        }
+    }
+
+    return 0;
+}
---
+++ lw2.3.c
@@ -0,0 +1,105 @@
+﻿// Функция получает строку, находит самый внутренний фрагмент в скобках и вырезает его. Операция повторяется до тех пор, пока не останется скобок. 
+// Полученные фрагменты и остаток строки вернуть в динамическом массиве указателей.
+
+#include <stdio.h>
+#include <locale.h>.
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#define MAXSTRING 100
+
+
+// вырезать фрагмент. Исходная строка меняется
+void cutFrag(char * str, int ptrFirst, int ptrLast) {
+	int countNum = 0;
+	int size = strlen(str);
+
+	for (int i = 0; i < size - ptrLast; i++) {
+		str[ptrFirst+i] = str[ptrLast + 1+i];
+	}
+}
+
+// найти пару скобке. Возвращает индекс парной скобки
+int findPair(char* str, int ptr) {
+	int indexStr = ptr;
+
+	while (str[indexStr] != '\0') {
+
+		if (str[indexStr] == ')') {
+			return indexStr;
+		}
+
+		indexStr++;
+	}
+}
+
+// найти последнюю скобку. Возвращает индекс первой скобки
+int findLast(char* str) {
+	int indexStr = 0;
+	int ptr = -1;
+
+	while (str[indexStr] != '\0') {
+		if (str[indexStr] == '(') {
+			ptr = indexStr;
+		}
+		indexStr++;
+	}
+
+	return ptr;
+}
+
+int main() {
+	char* locale = setlocale(LC_ALL, "");
+
+	// Строка должна быть составлена корректно. Каждая скобка должна иметь пару и пара должна начинаться с открывающейся скобки.
+	char str[MAXSTRING] = "(qw(jt)g(ldv))dsa\0";
+	printf("Исходная строка: %s\n", &str);
+
+	char** arr = (char**)malloc(sizeof(char*));
+	int countNum = 0; // счётчик количества найденных фрагментов (пар скобок)
+
+	// бесконечный цикл. Выход - отсутствие открывающихся скобок
+	while (1) {
+
+		int start = findLast(&str); // индекс открывающейся скобки
+
+		if (start == -1) {
+			//выход из цикла, если скобки закончились (как минимум нет открывающейся)
+			break;
+		}
+
+		int end = findPair(&str, findLast(&str)); // индекс закрывающейся
+
+		//формирование фрагмента
+		char val[MAXSTRING];
+		for (int i = 0; i < end-start-1; i++) {
+			val[i] = str[start + i+1];
+		}
+		val[end-start] = '\0';
+
+		arr[countNum] = (char*)malloc(sizeof(char*) * (strlen(val) + 1));
+
+		strcpy_s(arr[countNum], strlen(val) + 1, val);
+
+		//вырезать найденный фрагмен твместе со скобками
+		cutFrag(&str, start, end);
+		countNum++;
+	}
+	
+
+	if (countNum == 0) {
+		printf("Фрагменты в строке не найдены");
+		return 1;
+	}
+	else {
+		printf("Найдено %d фрагментов:\n", countNum);
+		for (int i = 0; i < countNum; i++) {
+			printf("%d фрагмент: %s\n", i + 1, arr[i]);
+		}
+		printf("Оставшаяся строка: %s\n", &str);
+	}
+
+	return 0;
+}
